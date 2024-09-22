c3dc38dd136b77b1a85e8500ba05933dad65fb55
lihui <826429402@qq.com>
2024-09-22 15:02:07 +0800

    go progamming language ch02 & ch04

--- ch02/boiling.go
+++ ch02/boiling.go
@@ -1,1 +1,11 @@
-package ch02
+package main
+
+import "fmt"
+
+const boilingF = 212.0
+
+func main() {
+	var f = boilingF
+	var c = (f - 32) * 5 / 9
+	fmt.Println("boiling point:", f, "or ", c, "C")
+}
